6f0ff1ce6540291150b458c7069abcaeec5074fa
vikramsingh <vc7662@gmail.com>
2025-06-30 17:57:34 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Vikram Singh Depawat on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
