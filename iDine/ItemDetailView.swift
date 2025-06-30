931e32de11571b798fa374349f015f033c9b2e9b
vikramsingh <vc7662@gmail.com>
2025-06-30 20:52:24 +0530

    i've add navigation stack and image, text, button to itemdetail screen

--- iDine/ItemDetailView.swift
+++ iDine/ItemDetailView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ItemDetailView: View {
+    var imageWidth: CGFloat {
+        UIScreen.main.bounds.width
+    }
+    var imageheight: CGFloat {
+        return imageWidth * 0.7
+    }
+    
     let item: MenuItem
     var body: some View {
-        Text(item.name)
+        NavigationStack {
+            VStack(spacing: 15) {
+                if !item.imageURL.isEmpty, let url = URL(string: item.imageURL) {
+                    AsyncImage(url: url)
+                        .frame(width: imageWidth, height: imageheight)
+                } else {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .frame(width: imageWidth, height: imageheight)
+                }
+                Text(item.description)
+                    .font(.body)
+                    .padding(.horizontal,16)
+               
+                Button("Order Now") {
+                    print("Login Button")
+                }
+                .font(.headline)
+                .foregroundStyle(.white)
+                .frame(width: 150, height: 44)
+                .background(.blue)
+                .cornerRadius(8)
+                .padding(.vertical)
+                
+                Spacer()
+            }
+            .navigationTitle(item.name)
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
